33a31c73e4ae0ccde7f9656b1dc4ffe8ff9680db
ViivekUV <46867694+ViivekUV@users.noreply.github.com>
2020-06-20 23:18:35 +0530

    Update upload_story.py

--- upload_story.py
+++ upload_story.py
@@ -5,10 +5,10 @@ from instabot import Bot
 bot = Bot()
 
 # Login to your account by typing in username and password
-bot.login(username='viivek_uvv', password='realmadridRONALDOINSTA007')
+bot.login(username='username', password='password')
 
 # Image path
-image = "./images/insta.jpg.STORIES.jpg"
+image = "./images/insta.jpg"
 
 # Upload the story as in your filepath, along with necessary caption 
 bot.upload_story_photo(image)
